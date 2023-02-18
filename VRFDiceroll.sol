d12501144f8ec975b2f0a24e7efba4198381c55f
Alabama Whiskey <amwtablet@gmail.com>
2023-02-18 14:56:32 -0500

    added mapping, rollDice function, and further code commenting to VRFDiceroll.sol

--- VRFDiceroll.sol
+++ VRFDiceroll.sol
@@ -13,23 +13,53 @@ contract VRFDiceroll is VRFConsumerBaseV2 {
     //Deployed VRF Coordinator Contract address from Chainlink
     address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
 
-    //
+    //hashed value of max gas price willing to pay (in wei), serves as an ID for the off-chain VRF job
     bytes32 s_keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
-
+    //limit  of gas to use for the callback request to the fulfillRandomWords function
     uint32 callbackGasLimit = 40000;
 
+    //number of confirmations required the Chainlink node should wait for before responding
+    uint16 requestConfirmations = 3; //must be greater than the minimumRequestBlockConfirmations limit on the coordinator contract
 
-    uint16 requestConfirmations = 3;
-
-
+    //number of random values requested per transaction 
     uint32 numWords =  1;
+
+    uint256 private constant ROLL_IN_PROGRESS = 42;
+
+    mapping(uint256 => address) private s_rollers;
+    mapping(address => uint256) private s_results;
+
+
+  //EVENTS
+    //event (state management) that signals that the dice is being rolled
+     //indexed refers to the fact that the value of this parameter will be stored on the blockchain and can be retrieved
+    event DiceRolled(uint256 indexed requestId, address indexed roller);
 
   //CONSTRUCTOR
     //also calls upon constructor from VRFConsumerBaseV2 (parent contract)
-    constructor(uint s_subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
-          COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+    constructor(uint subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+      COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+      s_owner = msg.sender; //set owner to the address that deployed the contract
+      s_subscriptionId = subscriptionId; //set subscription ID to the ID that was passed in
+    }
 
+  //FUNCTIONS
+    function rollDice(address roller) public onlyOwnwer returns (uint256 requestId) {
+      require(s_results[roller] == 0 , "Already rolled"); //require that the roller has not already rolled
+      requestId = COORDINATOR.requestRandomWords(
+        s_keyHash,
+        callbackGasLimit, 
+        s_subscriptionId,
+        roller,
+        requestConfirmations,
+        numWords
+      );
+
+      s_rollers[requestId] = roller;
+      s_results[roller] = ROLL_IN_PROGRESS;
+
+      emit DiceRolled(requestId, roller);
     }
 
 }
