d49b5448b294118d440c3734e5ada7e26244641d
Alabama Whiskey <amwtablet@gmail.com>
2023-02-18 15:23:03 -0500

    fulfillRandomWords functionality added

--- VRFDiceroll.sol
+++ VRFDiceroll.sol
@@ -1,3 +1,6 @@
+//This contract is a dice roller that uses Chainlink VRF to generate random numbers
+//Deployable on the Ethereum Sepolia testnet
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
@@ -33,8 +36,11 @@ contract VRFDiceroll is VRFConsumerBaseV2 {
 
   //EVENTS
     //event (state management) that signals that the dice is being rolled
-     //indexed refers to the fact that the value of this parameter will be stored on the blockchain and can be retrieved
+    //indexed refers to the fact that the value of this parameter will be stored on the blockchain and can be retrieved
     event DiceRolled(uint256 indexed requestId, address indexed roller);
+
+    //event (state management) that signals that the dice has landed
+    event DiceLanded(uint256 indexed requestId, uint256 indexed result);
 
   //CONSTRUCTOR
     //also calls upon constructor from VRFConsumerBaseV2 (parent contract)
@@ -60,6 +66,18 @@ contract VRFDiceroll is VRFConsumerBaseV2 {
       s_results[roller] = ROLL_IN_PROGRESS;
 
       emit DiceRolled(requestId, roller);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+
+      //transform the result to a number between 1 and 20, inclusively
+      uint256 d20vlaue = (randomWords[0] % 20) + 1;
+
+      //assign the transformed value to the address in the s_results mapping variable
+      s_results[s_rollers[requestId]] = d20value;
+
+      //emit event to signal that the dice has landed
+      emit DiceLanded(requestId, d20value);
     }
 
 }
